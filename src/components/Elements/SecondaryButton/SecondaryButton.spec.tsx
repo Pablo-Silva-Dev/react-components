f1065eb8127664132ac66f88e4e53ebb2b4c8969
pablolucio97 <pablolucio97@users.noreply.github.com>
2022-05-03 07:45:58 -0300

    ✔ Tests: Added tests to PrimaryButton and SecondaryButton components

---
+++ src/components/Elements/SecondaryButton/SecondaryButton.spec.tsx
@@ -0,0 +1,87 @@
+import React, { ReactNode, useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { render, screen, fireEvent } from '@testing-library/react'
+
+import { theme } from '../../../themes/theme'
+import { SecondaryButton } from '.'
+
+interface ChildrenProps {
+    children: ReactNode
+}
+
+const StyledProvider: React.FC = ({ children }: ChildrenProps) => {
+    return (
+        <ThemeProvider theme={theme}>
+            {children}
+        </ThemeProvider>
+    )
+}
+
+
+
+
+describe('SecondaryButton', () => {
+
+
+    it('should render correctly', () => {
+
+    
+        render(
+            <SecondaryButton
+                title='SecondaryButton'
+                onClick={() => { }}
+            />, {
+            wrapper: StyledProvider
+        }
+
+        )
+
+
+        const component = screen.getByText('SecondaryButton')
+
+        expect(component).toBeInTheDocument()
+
+    })
+
+    it('should be callled on click', () => {
+
+        const fn = jest.fn()
+
+        render(
+            <SecondaryButton
+                title='SecondaryButton'
+                onClick={fn}
+
+            />, {
+            wrapper: StyledProvider
+        }
+        )
+
+        const component = screen.getByText('SecondaryButton')
+
+        fireEvent.click(component)
+
+        expect(fn).toHaveBeenCalled()
+
+    })
+
+    it('should be disabled at loading is true', () => {
+        render(
+            <SecondaryButton
+                title='SecondaryButtonDisabled'
+                onClick={() => { }}
+                disabled={true}
+            />, {
+            wrapper: StyledProvider
+        }
+
+        )
+
+        const component = screen.getByRole('button', {name: /SecondaryButtonDisabled/i})
+
+        expect(component).toBeDisabled()
+
+
+
+    })
+})
