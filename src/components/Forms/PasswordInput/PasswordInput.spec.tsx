155c8c872b247466e8fd35f16f4cb06f903ddd81
pablolucio97 <pablolucio97@users.noreply.github.com>
2022-05-08 17:02:29 -0300

    Tests: Added test to PasswordInput component

---
+++ src/components/Forms/PasswordInput/PasswordInput.spec.tsx
@@ -0,0 +1,55 @@
+import { render, screen } from '@testing-library/react'
+import { ReactNode } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../../../themes/theme'
+import { PasswordInput } from '.'
+
+interface ChildrenProps {
+    children: ReactNode
+}
+
+const StyledProvider: React.FC = ({ children }: ChildrenProps) => {
+
+    return (
+        <ThemeProvider theme={theme}>
+            {children}
+        </ThemeProvider>
+    )
+}
+
+describe('PasswordInput', () => {
+    it('should render correctly', () => {
+        render(
+            <PasswordInput
+                showsIcon
+                passwordIsVisible={false}
+                togglePassword={() => { }}
+                data-testid='passwordinput-id'
+            />, {
+            wrapper: StyledProvider
+        }
+        )
+
+        const component = screen.getByTestId('passwordinput-id')
+
+        expect(component).toBeInTheDocument()
+
+    })
+    it('should be as password type when not visible', () => {
+        render(
+            <PasswordInput
+                showsIcon
+                passwordIsVisible={false}
+                togglePassword={() => { }}
+                data-testid='passwordinput-id'
+            />, {
+            wrapper: StyledProvider
+        }
+        )
+
+        const component = screen.getByTestId('passwordinput-id')
+
+        expect(component).toHaveAttribute('type', 'password')
+
+    })
+})
