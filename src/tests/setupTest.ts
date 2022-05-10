c54e7ad8e7d0636acabe24b3be19160f4c32c086
pablolucio97 <pablolucio97@users.noreply.github.com>
2022-05-10 07:08:53 -0300

    ✔ Tests: Added tests to ImageSlider component

--- src/tests/setupTest.ts
+++ src/tests/setupTest.ts
@@ -1,2 +1,16 @@
-import '@testing-library/jest-dom'
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+
+//for React Slick tests
+//@ts-ignore
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+      media: {},
+      onchange: () => {},
+    };
+  };
