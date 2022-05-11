d012fddcfce9e92589fd2608ea94d4636561c44a
pablolucio97 <pablolucio97@users.noreply.github.com>
2022-05-11 12:20:48 -0300

    ✔ Tests: Added tests to CollapsibleInfo component

---
+++ src/components/Elements/CollapsibleInfo/CollpasibleInfo.spec.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react'
+import { ReactNode } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../../../themes/theme'
+import { CollapsibleInfo } from './'
+
+interface ChildrenProps {
+    children: ReactNode
+}
+
+const StyledProvider: React.FC = ({ children }: ChildrenProps) => {
+    return (
+        <ThemeProvider theme={theme}>
+            {children}
+        </ThemeProvider>
+    )
+}
+
+describe('CollapsibleInfo', () => {
+    it('should render correctly', () => {
+        const { debug } = render(
+            <CollapsibleInfo
+                title='collapsibleinfo-title'
+                content='collapsibleinfo-content'
+            />, {
+            wrapper: StyledProvider
+        }
+        )
+
+        const component = screen.getByText('collapsibleinfo-content')
+        expect(component).toBeInTheDocument()
+    })
+})
