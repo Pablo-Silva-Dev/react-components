dcd66cd95ba321c2d36c638fd9c0c29472403ec3
pablolucio97 <pablolucio97@users.noreply.github.com>
2022-05-16 12:32:01 -0300

    ✔ Tests: Added tests to NewsletterCard component

---
+++ src/components/Elements/Cards/NewsletterCard/NewsletterCard.spec.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react'
+import { ReactNode } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../../../../themes/theme'
+import { NewsletterCard } from './'
+
+interface ChildrenProps {
+    children: ReactNode
+}
+
+const StyledProvider: React.FC = ({ children }: ChildrenProps) => {
+    return (
+        <ThemeProvider theme={theme}>
+            {children}
+        </ThemeProvider>
+    )
+}
+
+describe('NewsletterCard', () => {
+    it('should render correctly', () => {
+        render(
+            <NewsletterCard
+                emailPlaceholder='newslettercard-email-placeholder'
+                namePlaceholder='newslettercard-name-placeholder'
+                submit={async () => { }}
+                title='newslettercard-title'
+            />, {
+            wrapper: StyledProvider
+        }
+        )
+        const component = screen.getByText('newslettercard-title')
+        expect(component).toBeInTheDocument()
+    })
+})
