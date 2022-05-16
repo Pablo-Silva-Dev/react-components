e99dc4f229b7603ae10326004e958447e239db22
pablolucio97 <pablolucio97@users.noreply.github.com>
2022-05-16 12:26:02 -0300

    ✔ Tests: Added tests to ProductCard component

---
+++ src/components/Elements/Cards/ProductCard/ProductCard.spec.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react'
+import { ReactNode } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../../../../themes/theme'
+import { ProductCard } from './'
+
+interface ChildrenProps {
+    children: ReactNode
+}
+
+const StyledProvider: React.FC = ({ children }: ChildrenProps) => {
+    return (
+        <ThemeProvider theme={theme}>
+            {children}
+        </ThemeProvider>
+    )
+}
+
+describe('ProductCard', () => {
+    it('should render correctly', () => {
+        const { debug } = render(
+            <ProductCard
+                addToCart={async () => { }}
+                addToFavorites={async () => { }}
+                imgAlt='productcard-img-alt'
+                imgUrl='/productcard-img-url'
+                price='R$99,78'
+                priceOnCredit='R$109,88'
+                ratings={1.5}
+                title='productcard-title'
+            />, {
+            wrapper: StyledProvider
+        }
+        )
+
+        debug()
+
+        const component = screen.getByText('productcard-title')
+        expect(component).toBeInTheDocument()
+
+    })
+})
