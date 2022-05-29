c2c23c7297bbf743be576d00b41de64d5ddf08d1
pablolucio97 <pablolucio97@users.noreply.github.com>
2022-05-29 16:29:36 -0300

    ? Tests: Added tests to  NavTitle and NavLink components

---
+++ src/components/Elements/Nav/NavLink/NavLink.spec.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react'
+import { ReactNode } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../../../../themes/theme'
+import { NavLink } from './'
+
+interface ChildrenProps {
+    children: ReactNode
+}
+
+const StyledProvider: React.FC = ({ children }: ChildrenProps) => {
+    return (
+        <ThemeProvider theme={theme}>
+            {children}
+        </ThemeProvider>
+    )
+}
+
+describe('NavLink', () => {
+    it('should render correctly', () => {
+        render(
+            <NavLink
+                content='navlink-content'
+                url='/'
+            />, {
+            wrapper: StyledProvider
+        }
+        )
+
+        const component = screen.getByText('navlink-content')
+        expect(component).toBeInTheDocument()
+
+    })
+})
