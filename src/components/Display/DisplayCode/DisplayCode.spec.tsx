6d0ef10cd8553cda40a3975f2770bdb44002f6c3
pablolucio97 <pablolucio97@users.noreply.github.com>
2022-05-29 16:45:58 -0300

    ? Tests: Added tests to DisplayCode and RadioGroup components

---
+++ src/components/Display/DisplayCode/DisplayCode.spec.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react'
+import { ReactNode } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../../../themes/theme'
+import { DisplayCode } from './'
+
+interface ChildrenProps {
+    children: ReactNode
+}
+
+const StyledProvider: React.FC = ({ children }: ChildrenProps) => {
+    return (
+        <ThemeProvider theme={theme}>
+            {children}
+        </ThemeProvider>
+    )
+}
+
+describe('DisplayCode', () => {
+    it('should render correctly', () => {
+        render(
+            <DisplayCode
+                //eslint-disable-next-line
+                children="<p>Code</p>"
+            />, {
+            wrapper: StyledProvider
+        }
+        )
+        const component = screen.getByText('Code')
+        expect(component).toBeInTheDocument()
+    })
+})
---
+++ src/components/Forms/RadioGroup/RadioGroup.spec.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react'
+import { ReactNode } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../../../themes/theme'
+import { RadioGroup } from './'
+
+interface ChildrenProps {
+    children: ReactNode
+}
+
+
+const options = [
+    { value: 'chocolate', label: 'chocolate' },
+    { value: 'cherry', label: 'cherry' },
+    { value: 'apple', label: 'apple' },
+]
+
+const StyledProvider: React.FC = ({ children }: ChildrenProps) => {
+    return (
+        <ThemeProvider theme={theme}>
+            {children}
+        </ThemeProvider>
+    )
+}
+
+describe('RadioGroup', () => {
+    it('should render correctly', () => {
+        const {debug} = render(
+            <RadioGroup
+                options={options}
+                onChange={() => {}}
+            />, {
+            wrapper: StyledProvider
+        }
+        )
+        const component = screen.getByText('apple')
+        expect(component).toBeInTheDocument()
+    })
+})
