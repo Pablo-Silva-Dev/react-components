659a0ae150570340746b5adfc4e2c8a3f2c97831
Pablo <pablolucio_@hotmail.com>
2022-06-06 10:08:18 -0300

    📦 Components: SocialCollection component created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 
 import { Container } from '../styles';
+import { TestComponent } from 'react-component-lib-psd'
 
 export default function Home() {
 
   return (
     <Container> 
-    
+        <TestComponent />
     </Container>
   )
 }
