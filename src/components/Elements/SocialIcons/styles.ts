5cf64875d856206f2a9d8cb500a318fd597f2b4e
Pablo <pablolucio_@hotmail.com>
2022-07-13 13:31:54 -0300

    test:Added some tests to SocialIcon component

--- src/components/Elements/SocialIcons/styles.ts
+++ src/components/Elements/SocialIcons/styles.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+interface IconProps {
+  iconsSize?: "tiny" | "small" | "medium" | "large";
+}
+
 export const Container = styled.div`
   display: flex;
   width: 240px;
