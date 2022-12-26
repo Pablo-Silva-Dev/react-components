9f5f4106f61944ba2f539475db3f22296400bd3e
pablolucio97 <pablolucio_@hotmail.com>
2022-12-26 16:39:22 -0300

    fix: fix typing PaginationComponent to deploy

--- src/pages/pagination.tsx
+++ src/pages/pagination.tsx
@@ -5,76 +5,77 @@ import { SubTitle } from "../components/Typography/SubTitle";
 import { Text } from "../components/Typography/Text";
 import { Title } from "../components/Typography/Title";
 import {
-    ComponentContainer,
-    Container,
-    DescriptionContainer,
-    PreviewContainer,
-    PropsContainer
+  ComponentContainer,
+  Container,
+  DescriptionContainer,
+  PreviewContainer,
+  PropsContainer
 } from "../styles";
 
 export default function Pagination() {
 
-    const props = {
-        imgUrl: 'Description: Path to find image. Type: String. Required.',
-        size: 'Description: Image size. Type: "small" | "medium" | "large". Default: "medium".',
-        style: 'Description: Image style. Type: CSS Properties.',
-        className: 'Description: ClassName. Type: String.'
-    }
+  const props = {
+    imgUrl: 'Description: Path to find image. Type: String. Required.',
+    size: 'Description: Image size. Type: "small" | "medium" | "large". Default: "medium".',
+    style: 'Description: Image style. Type: CSS Properties.',
+    className: 'Description: ClassName. Type: String.'
+  }
 
-    const [page, setPage] = useState(1)
+  const [page, setPage] = useState(1)
 
-    const fakeData = Array.from(Array(40).keys())
-    return (
-        <Container>
-            <ComponentContainer>
-                <DescriptionContainer>
-                    <Title
-                        content='Pagination'
-                    />
-                    <Text
-                        content='Used to display user profile image.'
-                    />
-                </DescriptionContainer>
-                <PreviewContainer>
-                    <PaginationComponent
-                        onPageChange={setPage}
-                        currentPage={page}
-                        totalOfItems={fakeData.length}
-                        itemsPerPage={5}
-                    />
-                </PreviewContainer>
+  const fakeData = Array.from(Array(40).keys())
+  return (
+    <Container>
+      <ComponentContainer>
+        <DescriptionContainer>
+          <Title
+            content='Pagination'
+          />
+          <Text
+            content='Used to display user profile image.'
+          />
+        </DescriptionContainer>
+        <PreviewContainer>
+          <PaginationComponent
+            //@ts-ignore
+            onPageChange={setPage}
+            currentPage={page}
+            totalOfItems={fakeData.length}
+            itemsPerPage={5}
+          />
+        </PreviewContainer>
 
-                <PropsContainer>
-                    <SubTitle
-                        content='Props:'
-                        style={{
-                            fontSize: 20,
-                            fontWeight: 'bold'
-                        }}
-                    />
-                    {Object.entries(props).map(prop => (
-                        <>
-                            <SubTitle
-                                content={prop[0]}
-                                style={{
-                                    fontSize: 16
-                                }}
-                            />
-                            <Text
-                                content={prop[1]}
-                            />
-                        </>
-                    ))}
-                </PropsContainer>
-                <DisplayCode
-                    //eslint-disable-next-line
-                    children="
+        <PropsContainer>
+          <SubTitle
+            content='Props:'
+            style={{
+              fontSize: 20,
+              fontWeight: 'bold'
+            }}
+          />
+          {Object.entries(props).map(prop => (
+            <>
+              <SubTitle
+                content={prop[0]}
+                style={{
+                  fontSize: 16
+                }}
+              />
+              <Text
+                content={prop[1]}
+              />
+            </>
+          ))}
+        </PropsContainer>
+        <DisplayCode
+          //eslint-disable-next-line
+          children="
                     <Pagination
                     imageUrl='https://www.pablosilvadev.com.br/Pagination.svg'
                     size='medium'
                 />"
-                />
-            </ComponentContainer>
-        </Container>
-    )
+        />
+      </ComponentContainer>
+    </Container>
+  )
 }
